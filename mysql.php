184722f0fb66ea4c2bd3fcfbeef7af0bd1190876
JohnnyYu0930 <a3660980@gmail.com>
2016-05-12 12:01:24 +0800

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -2,9 +2,9 @@
  class Datebase {
 	public static function initDB() {
 		$db_host = 'db.mis.kuas.edu.tw';
-		$db_user = 's1103137224';
-		$db_pass = 'root';
-		$db_name = 's1103137224';
+		$db_user = ''; //帳號
+		$db_pass = ''; //密碼
+		$db_name = ''; //資料庫名稱
 		$dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
 		$db = new PDO($dsn, $db_user, $db_pass);
 		return $db;
